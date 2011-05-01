15ee097b850a2c966463b1ecda88a52f77857f2f
Graham McMillan <mcmgraham@gmail.com>
2011-04-30 22:31:21 -0400

    Fixed flash message js, using script.aculo.us

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+Event.observe(window, 'load', function() {
+    fadeout.delay(1.0);
+    Element.hide.delay(3.0, "notice");
+    Element.hide.delay(3.0, "alert");
+});
+
+function fadeout(){
+    new Effect.Opacity("notice", {duration:1.5, from:1.0, to:0.0});
+    new Effect.Opacity("alert", {duration:1.5, from:1.0, to:0.0});
+    
+}
